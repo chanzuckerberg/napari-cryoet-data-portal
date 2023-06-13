6e50238576a58d6d0afc3edf0f2b2c545ca6808a
Andy Sweet <andrew.d.sweet@gmail.com>
2023-06-13 11:52:35 -0700

    Increase coverage for URI widget

--- src/napari_cryoet_data_portal/_tests/test_uri_widget.py
+++ src/napari_cryoet_data_portal/_tests/test_uri_widget.py
@@ -52,5 +52,49 @@ def test_click_connect_when_uri_exists(widget: UriWidget, qtbot: QtBot, mocker: 
     assert not widget._connect_button.isVisibleTo(widget)
     assert not widget._choose_dir_button.isVisibleTo(widget)
     assert widget._uri_edit.isVisibleTo(widget)
+    assert widget._uri_edit.isReadOnly()
     assert widget._disconnect_button.isVisibleTo(widget)
     assert not widget._progress.isVisibleTo(widget)
+
+
+def test_click_connect_when_uri_does_not_exist(widget: UriWidget, qtbot: QtBot, mocker: MockerFixture):
+    mocker.patch('napari_cryoet_data_portal._uri_widget.path_exists', mock_path_exists)
+    widget._uri_edit.setText('s3://mock-bad-uri')
+
+    with qtbot.captureExceptions() as exceptions:
+        widget._connect_button.click()
+        qtbot.waitUntil(widget._connect_button.isEnabled)
+    assert len(exceptions) == 1
+    assert exceptions[0][0] is ValueError
+
+    assert widget._connect_button.isVisibleTo(widget)
+    assert widget._choose_dir_button.isVisibleTo(widget)
+    assert widget._uri_edit.isVisibleTo(widget)
+    assert not widget._uri_edit.isReadOnly()
+    assert not widget._disconnect_button.isVisibleTo(widget)
+    assert not widget._progress.isVisibleTo(widget)
+
+
+def test_click_disconnect(widget: UriWidget, qtbot: QtBot):
+    widget._onUriChecked((True, MOCK_S3_URI))
+
+    with qtbot.waitSignal(widget.disconnected):
+        widget._disconnect_button.click()
+
+    assert widget._connect_button.isVisibleTo(widget)
+    assert widget._choose_dir_button.isVisibleTo(widget)
+    assert widget._uri_edit.isVisibleTo(widget)
+    assert not widget._uri_edit.isReadOnly()
+    assert not widget._disconnect_button.isVisibleTo(widget)
+    assert not widget._progress.isVisibleTo(widget)
+
+
+def test_choose_dir_button_clicked(widget: UriWidget, mocker: MockerFixture):
+    mock_dir = '/path/to/test'
+    def mock_get_existing_directory(_):
+        return mock_dir
+    mocker.patch('napari_cryoet_data_portal._uri_widget.QFileDialog.getExistingDirectory', mock_get_existing_directory)
+
+    widget._choose_dir_button.click()
+
+    assert widget._uri_edit.text() == mock_dir
