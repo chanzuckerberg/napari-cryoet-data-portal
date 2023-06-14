2e57362b237d614d5af36cc108a288ec883c11be
Andy Sweet <andrew.d.sweet@gmail.com>
2023-06-14 14:35:35 -0700

    Add unit tests for main widget

--- src/napari_cryoet_data_portal/_tests/test_widget.py
+++ src/napari_cryoet_data_portal/_tests/test_widget.py
@@ -1,10 +1,11 @@
-from typing import Callable
+from typing import Callable, List, Tuple
 
 import pytest
 from napari import Viewer
 from napari.components import ViewerModel
 from pytest_mock import MockerFixture
 from pytestqt.qtbot import QtBot
+from qtpy.QtWidgets import QWidget
 
 from napari_cryoet_data_portal import DataPortalWidget
 
@@ -15,6 +16,16 @@ def widget(qtbot) -> DataPortalWidget:
     widget = DataPortalWidget(viewer)
     qtbot.add_widget(widget)
     return widget
+
+
+def show_all_sub_widgets(widget: DataPortalWidget) -> Tuple[QWidget, ...]:
+    layout = widget.layout()
+    sub_widgets: List[QWidget] = []
+    for i in range(layout.count()):
+        if w := layout.itemAt(i).widget():
+            sub_widgets.append(w)
+            w.show()
+    return tuple(sub_widgets)
 
 
 def test_init(qtbot: QtBot):
@@ -36,6 +47,17 @@ def test_add_widget_to_napari(make_napari_viewer: Callable[[], Viewer]):
     assert isinstance(widget, DataPortalWidget)
 
 
+def test_listing_item_changed_to_none(widget: DataPortalWidget, mocker: MockerFixture):
+    sub_widgets = show_all_sub_widgets(widget)
+
+    widget._listing.tree.currentItemChanged.emit(None, None)
+
+    assert all(
+        w.isVisibleTo(widget) == (w in (widget._uri, widget._listing))
+        for w in sub_widgets
+    )
+
+
 def test_connected_loads_listing(widget: DataPortalWidget, mocker: MockerFixture):
     mocker.patch.object(widget._listing, 'load')
     uri = 's3://mock-portal'
@@ -43,3 +65,14 @@ def test_connected_loads_listing(widget: DataPortalWidget, mocker: MockerFixture
     widget._uri.connected.emit(uri)
 
     widget._listing.load.assert_called_once_with(uri)
+
+
+def test_disconnected_only_shows_uri(widget: DataPortalWidget):
+    sub_widgets = show_all_sub_widgets(widget)
+
+    widget._uri.disconnected.emit()
+
+    assert all(
+        w.isVisibleTo(widget) == (w is widget._uri)
+        for w in sub_widgets
+    )
